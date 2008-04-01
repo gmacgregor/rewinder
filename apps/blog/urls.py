7b9413a1e153e484e625a30905ce2dc2dd6b029b
gmacgregor <gmacgregor@c7ad7044-cb60-498d-a845-55a1d3ae732a>
2008-04-01 03:37:09 +0000

    converted tabs to spaces, messed with tagging for Articles, modified article_detail template

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -2,17 +2,17 @@ from django.conf.urls.defaults import *
 from rewinder.apps.blog.models import Article
 
 article_dict = {
-	'queryset': Article.published_articles.all(),
-	'date_field': 'pub_date',
+    'queryset': Article.published_articles.all(),
+    'date_field': 'pub_date',
 }
 
 urlpatterns = patterns('django.views.generic.date_based',
-	url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/(?P<slug>[-\w]+)/$', 'object_detail', dict(article_dict, month_format='%m', template_object_name='article'), name='blog_entry_detail'),
-	url(r'^(?P<year>\d{4})/(?P<month>\d{2})/$', 'archive_month', dict(article_dict, month_format='%m'), name='blog_month_archive'),
-	url(r'^(?P<year>\d{4})/$', 'archive_year', dict(article_dict, make_object_list=True), name='blog_year_archive'),
-	url(r'^$', 'archive_index', dict(article_dict, num_latest=5), name='blog_home'),
+    url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/(?P<slug>[-\w]+)/$', 'object_detail', dict(article_dict, month_format='%m', template_object_name='article'), name='blog_entry_detail'),
+    url(r'^(?P<year>\d{4})/(?P<month>\d{2})/$', 'archive_month', dict(article_dict, month_format='%m'), name='blog_month_archive'),
+    url(r'^(?P<year>\d{4})/$', 'archive_year', dict(article_dict, make_object_list=True), name='blog_year_archive'),
+    url(r'^$', 'archive_index', dict(article_dict, num_latest=5), name='blog_home'),
 )
 
 #urlpatterns += patterns('django.views.generic.simple',
-#	(r'^$', 'direct_to_template', {'template': 'blog_home.html'}),
+#   (r'^$', 'direct_to_template', {'template': 'blog_home.html'}),
 #)
--- apps/blog/views.py
+++ apps/blog/views.py
@@ -7,27 +7,27 @@ from rewinder.apps.blog.models import Article
 
 #blog/
 def blog_home(request):
-	return render_to_response('home.html')
+    return render_to_response('home.html')
 #blog/archive/
 def archive_view(request):
-	return HttpResponse('This is archive_view')
+    return HttpResponse('This is archive_view')
 
 #blog/2007
 def year_view(request, year):
-	return HttpResponse('This is year_view')
+    return HttpResponse('This is year_view')
 
-#blog/2007/09	
+#blog/2007/09   
 def month_view(request, year, month):
-	return HttpResponse('This is month_view')
+    return HttpResponse('This is month_view')
 
-#blog/2007/09/29		
+#blog/2007/09/29        
 def day_view(request, year, month, day):
-	#articles = Article.objects.dates('pub_date', 'year')
-	return HttpResponse('This is day_view')
+    #articles = Article.objects.dates('pub_date', 'year')
+    return HttpResponse('This is day_view')
 
-#blog/2007/09/29/article-slug	
+#blog/2007/09/29/article-slug   
 def detail_view(request, year, month, day, slug):
-	u = User.objects.get(username='test')
-	article = get_object_or_404(Article, slug=slug)
-	Article.make_new_article(article)
-	return HttpResponse('it is done again agan')	
+    u = User.objects.get(username='test')
+    article = get_object_or_404(Article, slug=slug)
+    Article.make_new_article(article)
+    return HttpResponse('it is done again agan')    
