7c874740d9f507cb0bb101c44d201f70cccf0472
gmacgregor <gmacgregor@c7ad7044-cb60-498d-a845-55a1d3ae732a>
2008-04-08 18:19:19 +0000

    redefined create_tumblelog_item so that all models can use it

--- lib/signals.py
+++ lib/signals.py
@@ -14,61 +14,36 @@ def get_ctype(instance):
 def create_tumblelog_item(sender, instance):
     '''
     If the instance exists, just "pass". Otherwise, create a new TumblelogItem
-    '''
-    ctype = get_ctype(instance)
-    pub_date = instance.pub_date
-    try:
-        item = TumblelogItem.objects.get(object_id=instance.id, content_type=ctype)
-    except ObjectDoesNotExist:
-        item = TumblelogItem(pub_date=pub_date, object_id=instance.id, content_type=ctype)
-        item.save()
-
-def create_article_tumblelog_item(sender, instance):
-    '''
-    If the article's status is PUBLISHED_STATUS, attempt to create a new TumblelogItem
+    
+    In the case of a blog post, if the article's status is PUBLISHED_STATUS, attempt to create a new TumblelogItem.
     If it's not (ie. it is DRAFT_STATUS), try to remove it from the Tumblelog
     '''
-    from rewinder.apps.blog.models import PUBLISHED_STATUS, Article
-    ctype = get_ctype(Article)
-    if int(instance.status) is PUBLISHED_STATUS:
-        try:
-            item = TumblelogItem.objects.get(object_id=instance.id, content_type=ctype)
-        except ObjectDoesNotExist:
-            item = TumblelogItem(pub_date=instance.pub_date, object_id=instance.id, content_type=ctype)
-            item.save()
-    else:
-        try:
-            item = TumblelogItem.objects.get(object_id=instance.id, content_type=ctype)
-            item.delete()
-        except ObjectDoesNotExist:
-            pass
-
-def create_twitter_tumblelog_item(sender, instance):
-    from rewinder.apps.twitter.models import Tweet
-    ctype = get_ctype(Tweet)
+    ctype = get_ctype(instance)
+    if ctype.name == "article":
+        pub_date = instance.pub_date
+        from rewinder.apps.blog.models import PUBLISHED_STATUS
+        if int(instance.status) is not PUBLISHED_STATUS:
+            try:
+                item = TumblelogItem.objects.get(object_id=instance.id, content_type=ctype)
+                item.delete()
+            except ObjectDoesNotExist:
+                pass
+        else:
+            try:
+                item = TumblelogItem.objects.get(object_id=instance.id, content_type=ctype)
+            except ObjectDoesNotExist:
+                item = TumblelogItem(object_id=instance.id, content_type=ctype, pub_date=pub_date)
+                item.save()
+    elif ctype.name == "tweet":
+        pub_date = instance.pub_time
+    elif ctype.name == "bookmark":
+        pub_date = instance.saved_date
+    elif ctype.name == "photo":
+        pub_date = instance.taken_date
     try:
         item = TumblelogItem.objects.get(object_id=instance.id, content_type=ctype)
     except ObjectDoesNotExist:
-        item = TumblelogItem(pub_date=instance.pub_time, object_id=instance.id, content_type=ctype)
-        item.save()   
-
-def create_link_tumblelog_item(sender, instance):
-    from rewinder.apps.delicious.models import Bookmark
-    ctype = get_ctype(Bookmark)
-    try: 
-        item = TumblelogItem.objects.get(object_id=instance.id, content_type=ctype)
-    except ObjectDoesNotExist:
-        item = TumblelogItem(pub_date=instance.saved_date, object_id=instance.id, content_type=ctype)
-        item.save()
-
-
-def create_photo_tumblelog_item(sender, instance):
-    from rewinder.apps.flickr.models import Photo
-    ctype = get_ctype(Photo)
-    try: 
-        item = TumblelogItem.objects.get(object_id=instance.id, content_type=ctype)
-    except ObjectDoesNotExist:
-        item = TumblelogItem(pub_date=instance.taken_date, object_id=instance.id, content_type=ctype)
+        item = TumblelogItem(object_id=instance.id, content_type=ctype, pub_date=pub_date)
         item.save()
 
 #for model in settings.TUMBLELOG_MODELS:
