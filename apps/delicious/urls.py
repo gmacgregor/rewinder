84b52ba24531a78d84da362395501093df56543b
gmacgregor <gmacgregor@c7ad7044-cb60-498d-a845-55a1d3ae732a>
2008-04-10 04:22:52 +0000

    ummm... buncha changes!

--- apps/delicious/urls.py
+++ apps/delicious/urls.py
@@ -4,15 +4,21 @@ from rewinder.apps.delicious.models import Bookmark
 bookmark_dict = {
     'queryset': Bookmark.objects.all(),
     'date_field': 'saved_date',
+    'month_format': '%m',
+}
+
+bookmark_archive_dict = {
+    'queryset': Bookmark.objects.all(),
+    'date_field': 'saved_date',   
 }
 
 urlpatterns = patterns('django.views.generic.list_detail',
-    url(r'^$', 'object_list', dict(queryset=Bookmark.objects.all(), paginate_by=10), name='link_list'),
+    url(r'^$', 'object_list', dict(queryset=Bookmark.objects.all(), paginate_by=10), name='bookmark_list'),
 )
 
 urlpatterns += patterns('django.views.generic.date_based',
-    url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/(?P<object_id>\d+)/$', 'object_detail', dict(bookmark_dict, month_format='%m'), name='link_detail'),
-    url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/$', 'archive_day', dict(bookmark_dict, month_format='%m'), name='link_day_archive'),
-    url(r'^(?P<year>\d{4})/(?P<month>\d{2})/$', 'archive_month', dict(bookmark_dict, month_format='%m'), name='link_month_archive'),
-    url(r'^(?P<year>\d{4})/$', 'archive_year', dict(bookmark_dict, make_object_list=True), name='link_year_archive'),
+    url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/(?P<slug>[-\w]+)/$', 'object_detail', dict(bookmark_dict), name='bookmark_detail'),
+    url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/$', 'archive_day', dict(bookmark_dict), name='bookmark_archive_day'),
+    url(r'^(?P<year>\d{4})/(?P<month>\d{2})/$', 'archive_month', dict(bookmark_dict), name='bookmark_archive_month'),
+    url(r'^(?P<year>\d{4})/$', 'archive_year', dict(bookmark_archive_dict, make_object_list=True), name='bookmark_archive_year'),
 )
--- util/syncr/sync_delicious.py
+++ util/syncr/sync_delicious.py
@@ -2,6 +2,6 @@ from rewinder.util.syncr.delicious import DeliciousSyncr
 
 def run():
     d = DeliciousSyncr('sixminutes', 'faerie')
-    #d.syncAll()
+    d.syncAll()
     # sync today's bookmarks
-    d.syncBookmarks()
+    #d.syncBookmarks()
