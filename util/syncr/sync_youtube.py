d8b4f9b4ddabfa245e2b62b1af432d798d337f1d
gmacgregor <gmacgregor@c7ad7044-cb60-498d-a845-55a1d3ae732a>
2008-04-12 04:07:32 +0000

    deleted unused apps, everything youtube

--- util/syncr/sync_youtube.py
+++
@@ -1,8 +0,0 @@
-from rewinder.util.syncr.youtube import YoutubeSyncr
-
-def run():
-    yt = YoutubeSyncr()
-    videos = yt.syncUserFavorites('gregor003')
-    for video in videos:
-        print video
-        print '*'*40
