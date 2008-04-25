b8ae5afda716fa9565903be9a4a6e3ee81e3ccb6
gmacgregor <gmacgregor@c7ad7044-cb60-498d-a845-55a1d3ae732a>
2008-04-25 04:48:58 +0000

    removed template_object_name: all detail pages (generic views.object_detail) should use "object" as the template_object_name so that they play nicely with commenting

--- apps/video/urls.py
+++ apps/video/urls.py
@@ -11,7 +11,7 @@ urlpatterns = patterns('django.views.generic.list_detail',
 )
 
 urlpatterns += patterns('django.views.generic.date_based',
-    url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/(?P<slug>[-\w]+)/$', 'object_detail', dict(video_dict, month_format='%m', template_object_name='video'), name="video_detail"),
+    url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/(?P<slug>[-\w]+)/$', 'object_detail', dict(video_dict, month_format='%m'), name="video_detail"),
     url(r'^(?P<year>\d{4})/(?P<month>\d{2})/$', 'archive_month', dict(video_dict, month_format='%m'), name="video_month_detail"),
     url(r'^(?P<year>\d{4})/$', 'archive_year', dict(video_dict, make_object_list=True), name="video_year_detail"),
 )
