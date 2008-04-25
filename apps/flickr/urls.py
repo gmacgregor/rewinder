396f603c4724f6529659a67e5924407c5ef134b1
gmacgregor <gmacgregor@c7ad7044-cb60-498d-a845-55a1d3ae732a>
2008-04-25 22:13:55 +0000

    removed template_object_name from detail pages, changed photos_home url to photos_list for consistency

--- apps/flickr/urls.py
+++ apps/flickr/urls.py
@@ -7,8 +7,8 @@ photo_dict = {
 }
 
 urlpatterns = patterns('django.views.generic.date_based',
-    url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/(?P<object_id>\d+)/$', 'object_detail', dict(photo_dict, month_format='%m', template_object_name='photo'), name='photo_detail'),
+    url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/(?P<object_id>\d+)/$', 'object_detail', dict(photo_dict, month_format='%m'), name='photo_detail'),
     url(r'^(?P<year>\d{4})/(?P<month>\d{2})/$', 'archive_month', dict(photo_dict, month_format='%m'), name='photo_month_archive'),
     url(r'^(?P<year>\d{4})/$', 'archive_year', dict(photo_dict, make_object_list=True), name='photo_year_archive'),
-    url(r'^$', 'archive_index', dict(photo_dict, num_latest=5), name='photos_home'),
+    url(r'^$', 'archive_index', dict(photo_dict, num_latest=5), name='photos_list'),
 )
