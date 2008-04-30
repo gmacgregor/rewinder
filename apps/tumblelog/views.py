0968e75d4a43cd5820519b8547cfdd72df9a83b9
gmacgregor <gmacgregor@c7ad7044-cb60-498d-a845-55a1d3ae732a>
2008-04-30 13:31:52 +0000

    changed tumblelog/views.py to make use of  stateful paginator

--- apps/tumblelog/views.py
+++ apps/tumblelog/views.py
@@ -1,7 +1,13 @@
-from rewinder.apps.tumblelog.models import TumblelogItem
 from django.shortcuts import render_to_response
+from django.views.generic.list_detail import object_list
+from django.template import RequestContext
+from rewinder.apps.tumblelog.models import TumblelogItem
+from rewinder.lib.stateful_paginator import DiggPaginator
 
-
-def home(request):
-    logs = TumblelogItem.objects.all()
-    return render_to_response('tumblelog/home.html', {'logs': logs})
+def list(request):
+    items = TumblelogItem.objects.all()
+    page = request.GET.get('page', 1)
+    paginator = DiggPaginator(items, 15, page=page, body=7, tail=2, padding=3) 
+    return render_to_response('tumblelog/tumblelogitem_list.html',
+        {'page': page, 'paginator': paginator},
+        context_instance=RequestContext(request))
