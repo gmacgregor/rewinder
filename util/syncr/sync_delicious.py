217c4104be188e1043f9f68ae07056ca6bfd1726
gmacgregor <gmacgregor@c7ad7044-cb60-498d-a845-55a1d3ae732a>
2008-05-01 21:49:08 +0000

    added favs method to sync_flickr, sync_delicious now calls syncRecent()

--- util/syncr/sync_delicious.py
+++ util/syncr/sync_delicious.py
@@ -4,5 +4,6 @@ from rewinder.util.syncr.delicious import DeliciousSyncr
 def run():
     d = DeliciousSyncr(settings.DELICIOUS_USERNAME, settings.DELICIOUS_PASSWORD)
     # sync today's bookmarks
-    #d.syncBookmarks()
-    d.syncAll()
+    #d.syncBookmarks
+    d.syncRecent()
+    #d.syncAll()
