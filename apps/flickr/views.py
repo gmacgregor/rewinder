1bb3c4efe5aba92bae92322244f6fddad5cc6ca4
gmacgregor <gmacgregor@c7ad7044-cb60-498d-a845-55a1d3ae732a>
2008-05-01 21:54:37 +0000

    added flickr/faves url, faves view

--- apps/flickr/views.py
+++ apps/flickr/views.py
@@ -1,9 +1,13 @@
-from rewinder.apps.flickr.models import Photo
+from rewinder.apps.flickr.models import Photo, FavoriteList
 from rewinder.lib.stateful_paginator import DiggPaginator
 from rewinder.lib.shortcuts import render_response
 
 def list(request):
-    items = Photo.objects.all().order_by('id')
+    items = Photo.objects.all().filter(owner='sixminutes').order_by('id')
     page = request.GET.get('page', 1)
     paginator = DiggPaginator(items, 10, page=page, body=7, tail=2, padding=3) 
     return render_response(request, 'flickr/photo_list.html', {'page': page, 'paginator': paginator})
+
+def faves(request):
+    faves = FavoriteList.objects.get(owner='sixminutes').photos.all()
+    return render_response(request, 'flickr/photo_faves.html', {'photos': faves})
