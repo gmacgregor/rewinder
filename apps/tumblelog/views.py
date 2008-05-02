e9937fe5681db746cd4242a2f950cbab60646806
gmacgregor <gmacgregor@c7ad7044-cb60-498d-a845-55a1d3ae732a>
2008-05-02 01:27:55 +0000

    deleted urls and views because they are not necessary anymore

--- apps/tumblelog/views.py
+++
@@ -1,9 +0,0 @@
-from rewinder.apps.tumblelog.models import TumblelogItem
-from rewinder.lib.stateful_paginator import DiggPaginator
-from rewinder.lib.shortcuts import render_response
-
-def list(request):
-    items = TumblelogItem.objects.all().order_by('-pub_date')
-    page = request.GET.get('page', 1)
-    paginator = DiggPaginator(items, 15, page=page, body=7, tail=2, padding=3) 
-    return render_response(request, 'tumblelog/tumblelogitem_list.html', {'page': page, 'paginator': paginator})
