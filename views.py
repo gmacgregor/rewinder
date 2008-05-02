2b04a38fa2878169e350a5752754b57d898dfa22
gmacgregor <gmacgregor@c7ad7044-cb60-498d-a845-55a1d3ae732a>
2008-05-02 02:34:37 +0000

    added tagging view functions to rewinder.views

--- views.py
+++ views.py
@@ -1,5 +1,10 @@
+from tagging.models import Tag, TaggedItem
 from rewinder.lib.stateful_paginator import DiggPaginator
 from rewinder.lib.shortcuts import render_response
+from rewinder.apps.blog.models import Article
+from rewinder.apps.delicious.models import Bookmark
+from rewinder.apps.video.models import Video
+from rewinder.apps.flickr.models import Photo
 
 def list(request, app, model, ordering='-pub_date'):
     """
@@ -10,3 +15,29 @@ def list(request, app, model, ordering='-pub_date'):
     paginator = DiggPaginator(items, 15, page=page, body=7, tail=2, padding=3)
     template_name = '%s/%s_list.html' % (app.lower(), model.__name__.lower())
     return render_response(request, template_name, {'page': page, 'paginator': paginator})
+
+def all_tags(request):
+    all_tags = Tag.objects.all()
+    count = all_tags.count()
+    #tags = {}
+    #for tag in all_tags:
+    #    tags[tag] = tag.items.count()
+    #items = tags.items()
+    #items = [(v, k) for (k, v) in items]
+    #items.sort()
+    #items.reverse()
+    return render_response(request, 'tag/tag_list.html', {'tags': all_tags, 'count': count})
+
+def tag_detail(request, tag):
+    articles = TaggedItem.objects.get_by_model(Article, tag)
+    links = TaggedItem.objects.get_by_model(Bookmark, tag)
+    videos = TaggedItem.objects.get_by_model(Video, tag)
+    photos = TaggedItem.objects.get_by_model(Photo, tag)
+    tag_dict = {
+        'tag': tag,
+        'articles': articles,
+        'links': links,
+        'videos': videos,
+        'photos': photos,
+    }
+    return render_response(request, 'tag/tag_detail.html', tag_dict)
