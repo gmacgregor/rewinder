4724e41658f92871f93c866e6de9305a973c7b5f
gmacgregor <gmacgregor@c7ad7044-cb60-498d-a845-55a1d3ae732a>
2008-05-06 13:19:27 +0000

    added next, previous helpers to twitter Tweet, added comment moderation to Tweet, changed tweet url names

--- apps/twitter/models.py
+++ apps/twitter/models.py
@@ -1,15 +1,17 @@
+from django.conf import settings
 from django.db import models
 from django.dispatch import dispatcher
 from django.db.models import signals
 from rewinder.util.timeconverter import time_to_settings
+from threadedcomments.moderation import CommentModerator, moderator
 from rewinder.lib.signals import create_tumblelog_item, kill_tumblelog_item
-
 
 class Tweet(models.Model):
     pub_time            = models.DateTimeField()
     twitter_id          = models.PositiveIntegerField()
     text                = models.TextField()
     user                = models.ForeignKey('TwitterUser')
+    enable_comments     = models.BooleanField(default=True)
     
     def __unicode__(self):
         return u'%s' % (self.text)
@@ -31,13 +33,24 @@ class Tweet(models.Model):
             self.pub_time = time_to_settings(self.pub_time)
         super(Tweet, self).save()
     
+    def _next_previous_helper(self, direction):
+        return getattr(self, 'get_%s_by_pub_time' % direction)
+    
+    @property
+    def get_next(self):
+        return self._next_previous_helper('next')
+    
+    @property
+    def get_previous(self):
+        return self._next_previous_helper('previous')
+    
     class Meta:
         ordering = ('-pub_time',)
         get_latest_by = 'pub_time'
     
     class Admin:
         date_hierarchy = 'pub_time'
-        list_display = ('user', 'pub_time', 'text')
+        list_display = ('user', 'pub_time', 'text', 'enable_comments')
 
 class TwitterUser(models.Model):
     screen_name         = models.CharField(max_length=50)
@@ -61,6 +74,14 @@ class TwitterUser(models.Model):
     class Admin:
         list_display = ('screen_name', 'name', 'location', 'numFriends', 'numFollowers')
 
+
+class TweetModerator(CommentModerator):
+    akismet = settings.COMMENTS_AKISMET
+    auto_close_field = 'pub_time'
+    close_after = settings.COMMENTS_CLOSE_AFTER
+    email_notification = settings.COMMENTS_EMAIL
+    enable_field = settings.COMMENTS_ENABLE_FIELD
+moderator.register(Tweet, TweetModerator)
 
 dispatcher.connect(create_tumblelog_item, sender=Tweet, signal=signals.post_save)
 dispatcher.connect(kill_tumblelog_item, sender=Tweet, signal=signals.post_delete)
