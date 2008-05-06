4f99d44c051ad7a4f2198e0ec8b22d9807741ad9
gmacgregor <gmacgregor@c7ad7044-cb60-498d-a845-55a1d3ae732a>
2008-05-06 13:19:27 +0000

    added next, previous helpers to twitter Tweet, added comment moderation to Tweet, changed tweet url names

--- apps/twitter/urls.py
+++ apps/twitter/urls.py
@@ -11,7 +11,7 @@ urlpatterns = patterns('',
 
 urlpatterns += patterns('django.views.generic.date_based',
     url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/(?P<object_id>\d+)/$', 'object_detail', dict(tweet_dict, date_field='pub_time', month_format='%m'), name='tweet_detail'),
-    url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/$', 'archive_day', dict(tweet_dict, date_field='pub_time', month_format='%m'), name='tweet_day_archive'),
-    url(r'^(?P<year>\d{4})/(?P<month>\d{2})/$', 'archive_month', dict(tweet_dict, date_field='pub_time', month_format='%m'), name='tweet_month_archive'),
-    url(r'^(?P<year>\d{4})/$', 'archive_year', dict(tweet_dict, date_field='pub_time', make_object_list=True), name='tweet_year_archive'),
+    url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/$', 'archive_day', dict(tweet_dict, date_field='pub_time', month_format='%m'), name='tweet_archive_day'),
+    url(r'^(?P<year>\d{4})/(?P<month>\d{2})/$', 'archive_month', dict(tweet_dict, date_field='pub_time', month_format='%m'), name='tweet_archive_month'),
+    url(r'^(?P<year>\d{4})/$', 'archive_year', dict(tweet_dict, date_field='pub_time', make_object_list=True), name='tweet_archive_year'),
 )
