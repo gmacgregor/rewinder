2d95accba612781bcded60f7d4ceb572f244f081
gmacgregor <gmacgregor@c7ad7044-cb60-498d-a845-55a1d3ae732a>
2008-05-06 22:05:21 +0000

    updated video.urls

--- apps/video/urls.py
+++ apps/video/urls.py
@@ -12,6 +12,7 @@ urlpatterns = patterns('',
 
 urlpatterns += patterns('django.views.generic.date_based',
     url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/(?P<slug>[-\w]+)/$', 'object_detail', dict(video_dict, month_format='%m'), name="video_detail"),
-    url(r'^(?P<year>\d{4})/(?P<month>\d{2})/$', 'archive_month', dict(video_dict, month_format='%m'), name="video_month_detail"),
-    url(r'^(?P<year>\d{4})/$', 'archive_year', dict(video_dict, make_object_list=True), name="video_year_detail"),
+    url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/$', 'archive_day', dict(video_dict, month_format='%m'), name='video_archive_day'),
+    url(r'^(?P<year>\d{4})/(?P<month>\d{2})/$', 'archive_month', dict(video_dict, month_format='%m'), name="video_archive_month"),
+    url(r'^(?P<year>\d{4})/$', 'archive_year', dict(video_dict, make_object_list=True), name="video_archive_year"),
 )
