4b831386515fa4424e9f2631486b72e24b4d4e10
gmacgregor <gmacgregor@c7ad7044-cb60-498d-a845-55a1d3ae732a>
2008-05-07 13:56:39 +0000

    removed Articles from tumblelog list view

--- apps/tumblelog/views.py
+++ apps/tumblelog/views.py
@@ -8,11 +8,11 @@ from rewinder.apps.video.models import Video
 from rewinder.views import list
 
 def list_items(request):
-    logs = TumblelogItem.objects.all().count()
-    articles = Article.published_articles.all().count()
+    logs = TumblelogItem.objects.all().exclude(content_type__exact=15).count()
+    #articles = Article.published_articles.all().count()
     links = Bookmark.objects.all().count()
     videos = Video.objects.all().count()
     photos = Photo.objects.all().filter(owner='sixminutes').count()
     tweets = Tweet.objects.all().count()
-    ctx = {'logs': logs, 'links': links, 'articles': articles, 'photos': photos, 'tweets': tweets, 'videos': videos}
+    ctx = {'logs': logs, 'links': links, 'photos': photos, 'tweets': tweets, 'videos': videos}
     return list(request, 'tumblelog', TumblelogItem, '-pub_date', ctx)
