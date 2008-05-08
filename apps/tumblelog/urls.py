b2d1c92b7be2c5c6a20cf631ec50cad36314d8f9
gmacgregor <gmacgregor@c7ad7044-cb60-498d-a845-55a1d3ae732a>
2008-05-08 23:07:45 +0000

    modified tumblelog/urls to use custom view, re-worked custom views

--- apps/tumblelog/urls.py
+++ apps/tumblelog/urls.py
@@ -11,11 +11,13 @@ urlpatterns = patterns('',
 )
 
 #urlpatterns += patterns('django.views.generic.date_based',
-#    url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/$', 'archive_day', dict(log_dict, month_format='%m'), name='tumblelog_archive_day'),
-#    url(r'^(?P<year>\d{4})/(?P<month>\d{2})/$', 'archive_month', dict(log_dict, month_format='%m'), name='tumblelog_archive_month'),
-#    url(r'^(?P<year>\d{4})/$', 'archive_year', dict(log_dict, make_object_list=True), name='tumblelog_archive_year'),
+    #url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/$', 'archive_day', dict(log_dict, month_format='%m'), name='tumblelog_archive_day'),
+    #url(r'^(?P<year>\d{4})/(?P<month>\d{2})/$', 'archive_month', dict(log_dict, month_format='%m'), name='tumblelog_archive_month'),
+    #url(r'^(?P<year>\d{4})/$', 'archive_year', dict(log_dict, make_object_list=True), name='tumblelog_archive_year'),
 #)
 
 urlpatterns += patterns('rewinder.apps.tumblelog.views',
-    url(r'^(?P<year>\d{4})/$', 'items_by_year')
+    #url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/$', 'items_by_day', name='tumblelog_archive_day'),
+    url(r'^(?P<year>\d{4})/(?P<month>\d{2})/$', 'items_by_month', name='tumblelog_archive_month'),
+    url(r'^(?P<year>\d{4})/$', 'items_by_year', name='tumblelog_archive_year')
 )
