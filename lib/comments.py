30329af6d40141118bd3dfe3421abeedc851b875
gmacgregor <gmacgregor@c7ad7044-cb60-498d-a845-55a1d3ae732a>
2008-05-09 20:12:25 +0000

    comments module added with a single function, close()...

---
+++ lib/comments.py
@@ -0,0 +1,14 @@
+from django.conf import settings
+from rewinder.apps.tumblelog.models import TumblelogItem
+import datetime
+
+
+def close():
+    today = datetime.datetime.today()
+    close_date = today - datetime.timedelta(days=settings.COMMENTS_CLOSE_AFTER)
+    objs = TumblelogItem.objects.all()
+    for o in objs:
+        if o.pub_date < close_date:
+            mod = o.get_content_object()
+            mod.enable_comments = False
+            mod.save()
