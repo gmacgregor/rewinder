dc3d0f0c88ae1000bd70cd1d727dff6c450371fd
gmacgregor <gmacgregor@c7ad7044-cb60-498d-a845-55a1d3ae732a>
2008-05-16 21:45:28 +0000

    fixed up tumblelog views

--- apps/tumblelog/views.py
+++ apps/tumblelog/views.py
@@ -8,12 +8,21 @@ def year_group(request, year):
     return render_response(request, 'tumblelog/year_group.html', {'entries': logs, 'showing_date': date})
 
 def month_group(request, year, month):
-    logs = TumblelogItem.objects.filter(pub_date__year=year).filter(pub_date__month=month).order_by('-pub_date')
-    date = datetime(int(year),int(month),1)
+    date = datetime.strptime("%s %s" % (year, month), "%Y %m")
+    # this is lame... i must be doing something wrong
+    month = _process_digit(month)
+    logs = TumblelogItem.objects.filter(pub_date__year=year, pub_date__month=month).order_by('-pub_date')
     return render_response(request, 'tumblelog/month_group.html', {'entries': logs, 'showing_date': date})
 
 def day_group(request, year, month, day):
-    logs = TumblelogItem.objects.filter(pub_date__year=year).filter(pub_date__month=month).filter(pub_date__day=day).order_by('-pub_date')
-    date = datetime(int(year),int(month),int(day))
+    date = datetime.strptime("%s %s %s" % (year, month, day), "%Y %m %d")
+    month = _process_digit(month)
+    day = _process_digit(day)
+    logs = TumblelogItem.objects.filter(pub_date__year=year, pub_date__month=month, pub_date__day=day).order_by('-pub_date')
     return render_response(request, 'tumblelog/day_group.html', {'entries': logs, 'showing_date': date})
+
+def _process_digit(value):
+    if value.startswith('0'):
+        value = value.split('0', 1)[1]
+    return value
     
