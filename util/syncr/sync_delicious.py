da84f07cae5a21b0cae8c7bc59ae788c3b584db8
gmacgregor <gmacgregor@c7ad7044-cb60-498d-a845-55a1d3ae732a>
2008-05-20 05:23:34 +0000

    changed delicious and flickr sync scripts to more reasonable values

--- util/syncr/sync_delicious.py
+++ util/syncr/sync_delicious.py
@@ -4,6 +4,4 @@ from rewinder.util.syncr.delicious import DeliciousSyncr
 def run():
     d = DeliciousSyncr(settings.DELICIOUS_USERNAME, settings.DELICIOUS_PASSWORD)
     # sync today's bookmarks
-    #d.syncBookmarks
-    #d.syncRecent()
-    d.syncAll()
+    d.syncRecent()
