463375b507cd0bae35e49a0b10d39fcd845de40c
gmacgregor <gmacgregor@c7ad7044-cb60-498d-a845-55a1d3ae732a>
2008-05-20 17:20:55 +0000

    added __name__ == "main" to comments.py

--- lib/comments.py
+++ lib/comments.py
@@ -14,12 +14,14 @@ from rewinder.apps.tumblelog.models import TumblelogItem
 def close():
     today = datetime.datetime.today()
     close_date = today - datetime.timedelta(days=settings.COMMENTS_CLOSE_AFTER)
+    print "closing comments %s days old (published before %s)" % (settings.COMMENTS_CLOSE_AFTER, close_date)
     objs = TumblelogItem.objects.all()
     for o in objs:
         if o.pub_date < close_date:
             mod = o.get_content_object()
             mod.enable_comments = False
             mod.save()
+    print "done"
 
 def enable():
     objs = TumblelogItem.objects.all()
@@ -27,3 +29,6 @@ def enable():
         mod = o.get_content_object()
         mod.enable_comments = True
         mod.save()
+
+if __name__ == "main":
+    close()
