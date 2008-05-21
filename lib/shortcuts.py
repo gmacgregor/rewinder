74f172cdfdc268255cf4b33d8eb47c74582a105b
victor.ng <victor.ng@c7ad7044-cb60-498d-a845-55a1d3ae732a>
2008-05-21 01:33:45 +0000

    added security comment to render_response re: csrf and xss security hole

--- lib/shortcuts.py
+++ lib/shortcuts.py
@@ -2,5 +2,7 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 
 def render_response(req, *args, **kwargs):
+    # This is bad.  You don't want to automagically inject request data into the response
+    # since it can open an XSS or CSRF security hole.
     kwargs['context_instance'] = RequestContext(req)
     return render_to_response(*args, **kwargs)
