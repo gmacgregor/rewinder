9ec3839f5ece66dae96ed9a07adaa4abf80c754d
victor.ng <victor.ng@c7ad7044-cb60-498d-a845-55a1d3ae732a>
2008-05-21 01:31:57 +0000

    updated the views to use datetime.strptime to parse out a datetime object, and then extract the integer portinos of year, month and day as attributes of the datetime instance

--- apps/tumblelog/views.py
+++ apps/tumblelog/views.py
@@ -3,26 +3,23 @@ from rewinder.lib.shortcuts import render_response
 from datetime import datetime
 
 def year_group(request, year):
+    date = datetime.strptime(year, "%Y")
+    year = date.year
     logs = TumblelogItem.objects.filter(pub_date__year=year).order_by('-pub_date')
-    date = datetime(int(year),1,1)
     return render_response(request, 'tumblelog/year_group.html', {'entries': logs, 'showing_date': date})
 
 def month_group(request, year, month):
     date = datetime.strptime("%s %s" % (year, month), "%Y %m")
     # this is lame... i must be doing something wrong
-    month = _process_digit(month)
+    month = date.month
     logs = TumblelogItem.objects.filter(pub_date__year=year, pub_date__month=month).order_by('-pub_date')
     return render_response(request, 'tumblelog/month_group.html', {'entries': logs, 'showing_date': date})
 
 def day_group(request, year, month, day):
     date = datetime.strptime("%s %s %s" % (year, month, day), "%Y %m %d")
-    month = _process_digit(month)
-    day = _process_digit(day)
+    month = date.month
+    day = date.day
     logs = TumblelogItem.objects.filter(pub_date__year=year, pub_date__month=month, pub_date__day=day).order_by('-pub_date')
     return render_response(request, 'tumblelog/day_group.html', {'entries': logs, 'showing_date': date})
 
-def _process_digit(value):
-    if value.startswith('0'):
-        value = value.split('0', 1)[1]
-    return value
-    
+
