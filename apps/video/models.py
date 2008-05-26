0a8517d63f7fa298c174a1c2c3bb6f897a45b858
gmacgregor <gmacgregor@c7ad7044-cb60-498d-a845-55a1d3ae732a>
2008-05-26 01:00:19 +0000

    changed youtube w/h again

--- apps/video/models.py
+++ apps/video/models.py
@@ -13,8 +13,9 @@ from rewinder.apps.tumblelog.models import TumblelogItem
 from rewinder.lib.signals import create_tumblelog_item, kill_tumblelog_item
 #from rewinder.util.timeconverter import time_to_utc
 
-YOUTUBE_WIDTH = 395
-YOUTUBE_HEIGHT = 480
+YOUTUBE_WIDTH = 480
+YOUTUBE_HEIGHT = 395
+
 EXTERNAL_VIDEO_WIDTH = 499
 EXTERNAL_VIDEO_HEIGHT = 300
 
