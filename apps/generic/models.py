c989e6d074c146dc267acf2892baef4f7059b440
gmacgregor <gmacgregor@c7ad7044-cb60-498d-a845-55a1d3ae732a>
2008-05-30 20:40:41 +0000

    added quote evolutions, modified generic Quote model

--- apps/generic/models.py
+++ apps/generic/models.py
@@ -2,8 +2,13 @@ from django.db import models
 from django.conf import settings
 from tagging.fields import TagField
 from template_utils.markup import formatter
+from typogrify.templatetags.typogrify import typogrify
 from comment_utils.moderation import CommentModerator, moderator
 
+from django.dispatch import dispatcher
+from django.db.models import signals
+from rewinder.apps.tumblelog.models import TumblelogItem
+from rewinder.lib.signals import create_tumblelog_item, kill_tumblelog_item
 
 class SourceCategory(models.Model):
     created_on          = models.DateTimeField(auto_now_add=True)
@@ -42,30 +47,45 @@ class Quote(models.Model):
     created_on          = models.DateTimeField(auto_now_add=True)
     last_modified       = models.DateTimeField(auto_now=True)
     pub_date            = models.DateTimeField('Publication Date')
-    author              = models.ForeignKey('Person', null=True, blank=True)
-    source              = models.ForeignKey(Source)
+    #author              = models.ForeignKey('Person', null=True, blank=True)
+    #source              = models.ForeignKey(Source)
     url                 = models.URLField(u'URL', blank=True, help_text=u'Optional.', verify_exists=False)
-    text                = models.TextField()
+    text                = models.TextField(help_text=u'Use Markdown syntax for HTML formatting.')
+    slug                = models.SlugField(null=True)
     html_text           = models.TextField(blank=True, editable=False)
     rating              = models.CharField(max_length=20, choices=settings.RATING_CHOICES, blank=True, help_text=u'Optional')
+    credit              = models.CharField(max_length=255, null=True, blank=True)
     tags                = TagField()
     enable_comments     = models.BooleanField(default=True)
     
     def __unicode__(self):
         return u'%s' % self.text
     
+    @models.permalink
+    def get_absolute_url(self):
+        return ('quote_detail', (), {
+            'year': self.pub_date.year,
+            'month': str(self.pub_date.month).zfill(2),
+            'day': str(self.pub_date.day).zfill(2),
+            'slug': self.slug,
+        })
+    
     def save(self):
-        self.html_text = formatter(self.text)
-            
+        self._process_markup()
+        super(Quote, self).save()
+    
+    def _process_markup(self):
+        self.html_text =  typogrify(formatter(self.text))
+    
     class Admin:
-        list_display    = ('text', 'author', 'source', 'url')
-        search_fields   = ['text', 'description']
+        list_display    = ('text', 'url', 'enable_comments')
+        search_fields   = ['text', 'credit']
 
 
 class QuoteModerator(CommentModerator):
     akismet = settings.COMMENTS_AKISMET
-    auto_close_field = 'pub_date'
-    close_after = settings.COMMENTS_CLOSE_AFTER
+    auto_moderate_field = 'pub_date'
+    moderate_after = settings.COMMENTS_MODERATE_AFTER
     email_notification = settings.COMMENTS_EMAIL
     enable_field = settings.COMMENTS_ENABLE_FIELD
 moderator.register(Quote, QuoteModerator)
@@ -105,3 +125,6 @@ class Series(models.Model):
     
     class Admin:
         pass
+        
+dispatcher.connect(create_tumblelog_item, sender=Quote, signal=signals.post_save)
+dispatcher.connect(kill_tumblelog_item, sender=Quote, signal=signals.post_delete)
