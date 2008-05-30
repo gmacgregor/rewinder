69615ff2200f155c21900325e737c83df026b5f6
gmacgregor <gmacgregor@c7ad7044-cb60-498d-a845-55a1d3ae732a>
2008-05-30 04:37:28 +0000

    added image fields, create a few new private methods

--- apps/delicious/models.py
+++ apps/delicious/models.py
@@ -2,23 +2,28 @@ from django.conf import settings
 from django.db import models
 from django.dispatch import dispatcher
 from django.db.models import signals
+from django.template.defaultfilters import slugify
 from threadedcomments.moderation import CommentModerator, moderator
 from tagging.fields import TagField
+from template_utils.markup import formatter
+from typogrify.templatetags.typogrify import typogrify
 
 from rewinder.util.timeconverter import time_to_settings
 from rewinder.lib.signals import create_tumblelog_item, kill_tumblelog_item
 
 import datetime
+import re
 
 class Bookmark(models.Model):
     saved_date              = models.DateTimeField(default=datetime.datetime.today)
     description             = models.CharField(max_length=255, blank=True)
-    slug                    = models.SlugField(max_length=255, prepopulate_from=('description',), help_text='Automatically built from description.', unique_for_date='saved_date') 
+    slug                    = models.SlugField(max_length=255, prepopulate_from=('description',), help_text='Automatically built from description.', unique_for_date='saved_date')
     url                     = models.URLField()
     tags                    = TagField()
-    #image                   = models.ImageField(upload_to='img/links/%Y%m/', blank=True)
-    #image_caption           = models.TextField(blank=True, help_text=u'Use Markdown syntax for HTML formatting.')
+    #image                   = models.ImageField(upload_to='img/links/%Y%m/', blank=True, null=True)
+    #image_caption           = models.TextField(blank=True, help_text=u'Use Markdown syntax for HTML formatting.', null=True)
     #html_image_caption      = models.TextField(blank=True, null=True)
+    #remove_image            = models.BooleanField(default=False, help_text='Check to remove image')
     extended_info           = models.CharField(max_length=255, blank=True)
     post_hash               = models.CharField(max_length=100, null=True, blank=True)
     via_url                 = models.URLField(u'Via URL', verify_exists=False, blank=True, null=True, help_text=u'The URL of the site where you spotted the link. Optional.')
@@ -49,31 +54,48 @@ class Bookmark(models.Model):
         return self._next_previous_helper('previous')
             
     def save(self):
-        """
-        If this link is being saved for the first time (ie. imported from del.icou.us):
-            1. create self.slug based on self.description
-            2. remove any tags that contain the characters: :+./&#
-            3. check if post_elsewhere is true, and if so, attempt to post to del.icio.us
-        """
         if not self.id:
-            import re
-            from django.template.defaultfilters import slugify
             self.slug = slugify(self.description)
             self.saved_date = time_to_settings(self.saved_date)
-            tags_re = re.compile('[\:|\+|\&|\/|\#|\.]+')
-            tags = self.tags.split()
-            new_tags = []
-            for tag in tags:
-                if not tags_re.search(tag):
-                    new_tags.append(tag)
-            self.tags = ' '.join(new_tags)
-            if self.post_elsewhere:
-                import pydelicious
-                try:
-                    pydelicious.add(settings.DELICIOUS_USERNAME, settings.DELICIOUS_PASSWORD, self.url, self.description, self.tags, self.extended_info)
-                except:
-                    pass
+        self._process_tags()
+        if self.remove_image:
+            self._process_image()
+        self._process_markup()
+        if self.post_elsewhere:
+            self._post_to_delicious()
         super(Bookmark, self).save()
+    
+    def _post_to_delicious(self):
+        import pydelicious
+        try:
+            pydelicious.add(settings.DELICIOUS_USERNAME, settings.DELICIOUS_PASSWORD, self.url, self.description, self.tags, self.extended_info)
+        except:
+            pass
+        return self
+    
+    def _process_tags(self):
+        tags_re = re.compile('[\:|\+|\&|\/|\#|\.]+')
+        tags = self.tags.split()
+        new_tags = []
+        for tag in tags:
+            if not tags_re.search(tag):
+                new_tags.append(tag)
+        self.tags = ' '.join(new_tags)
+        return self
+    
+    def _process_markup(self):
+        self.html_image_caption =  typogrify(formatter(self.image_caption))
+        return self
+    
+    def _process_image(self):
+        try:
+            os.remove(self.get_image_filename())
+        except OSError:
+            # most likely that image doesn't exist
+            pass
+        self.image, self.image_caption, self.html_image_caption = '', '', ''
+        self.remove_image = False
+        return self
     
     class Meta:
         ordering = ('-saved_date',)
