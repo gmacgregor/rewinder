97f442ad47c88c89ec43eec01181a0af20f62104
gmacgregor <gmacgregor@c7ad7044-cb60-498d-a845-55a1d3ae732a>
2009-01-05 04:24:10 +0000

    Removed unnecessary scripts, modifed sync_ files

--- util/syncr/sync_delicious.py
+++ util/syncr/sync_delicious.py
@@ -1,7 +1,8 @@
 from django.conf import settings
-from rewinder.util.syncr.delicious import DeliciousSyncr
+from rewinder.syncr.app.delicious import DeliciousSyncr
 
 def run():
     d = DeliciousSyncr(settings.DELICIOUS_USERNAME, settings.DELICIOUS_PASSWORD)
     # sync today's bookmarks
-    d.syncRecent()
+    #d.syncRecent()
+    d.syncAll()
